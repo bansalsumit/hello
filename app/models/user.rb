3521d20d3dc384bb32b98772431c9a31985017d7
sumit bansal <sumit.bansal@loconav.com>
2018-08-30 11:22:52 +0530

    micropost added for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+  default_scope -> { order(created_at: :desc) }
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
+  resources :microposts,          only: [:create, :destroy]
 end
