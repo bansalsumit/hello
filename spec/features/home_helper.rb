5c79eee890a3e07a67be2c584b7dcdea8a98ea79
sumit bansal <sumit.bansal@loconav.com>
2018-09-01 22:42:04 +0530

    test commit

--- spec/features/home_helper.rb
+++ spec/features/home_helper.rb
@@ -1,5 +1,7 @@
+require 'capybara/dsl'
+Capybara.default_driver = :selenium
 feature "the signin process", type: :feature do
   scenario "signs me in" do
-    visit '/login'
+    visit '/help'
   end
 end
